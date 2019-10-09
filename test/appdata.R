4c7c4d80da9bc16b85ba5be963e69eb2f7678d76
yyfyifan <125140762@qq.com>
2019-10-08 23:51:58 -0400

    Deleted useless example files, added a deploy instruction

--- test/appdata.R
+++
@@ -1,5 +0,0 @@
-APP_DATA_DIR = "~/Developer/fall2019-proj2--sec1-grp5/data/"
-
-dog_license_df <- read.csv(file.path(APP_DATA_DIR, "dog_licensing.csv"))
-dog_bite_df <- read.csv(file.path(APP_DATA_DIR, "dog-bite.csv"))
-parks_df <- read.csv(file.path(APP_DATA_DIR, "parks.csv"))
